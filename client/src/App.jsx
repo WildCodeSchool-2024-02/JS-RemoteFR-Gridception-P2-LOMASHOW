ae04e5289a51fef88a5437e4efaf0b8d9f064ca7
Maxime MAUSSION <maximemaussion30@gmail.com>
2024-04-10 09:55:56 +0200

    Added the correct way to import images, created a way to protect our api key

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,44 +6,41 @@ function App() {
   const [datas, setDatas] = useState();
 
   const options = {
-    method: 'GET',
-    url: 'https://api.themoviedb.org/3/movie/top_rated',
-    params: { language: 'en-US', page: '1' },
-    headers: { accept: 'application/json' }
+    method: "GET",
+    url: "https://api.themoviedb.org/3/movie/top_rated",
+    params: { language: "en-US", page: "1" },
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
+    },
   };
 
   useEffect(() => {
     axios
       .request(options)
-      .then(function (response) {
-
-        setDatas(response.data)
+      .then((response) => {
+        setDatas(response.data);
       })
-      .catch(function (error) {
+      .catch((error) => {
         console.error(error);
       });
-
-  }, [])
-  console.log(datas);
-
-
+  }, []);
+  console.info(datas);
 
   return (
     <div>
       <figcaption>
-        <img src={datas?.results[2]?.poster_path} alt={datas?.results[2]?.title} />
         <figure>
-          <p>TOTO</p>
-          {datas?.results[2]?.title}
+          <img
+            src={`https://image.tmdb.org/t/p/w500/${datas?.results[2]?.poster_path}`}
+            alt={datas?.results[2]?.vote_average}
+          />
         </figure>
+        {datas?.results[2]?.title}
+        <p>{datas?.results[2]?.overview}</p>
       </figcaption>
     </div>
   );
 }
 
-
-
 export default App;
-
-
-

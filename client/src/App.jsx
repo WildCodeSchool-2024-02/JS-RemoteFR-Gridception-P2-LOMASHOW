6238baf5e1ea4422f91aa4bff5c11a7c74c7bc39
Maxime MAUSSION <maximemaussion30@gmail.com>
2024-04-26 12:41:30 +0200

    fixed swipe animation, now contain desc

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
+
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "./context/AuthContext";
 
 import MovieCard from "./components/MovieCard/MovieCard";
 import Movinder from "./assets/images/MovInder175.png";
+import Like from "./components/Like/Like";
 
 import Swipeable from "./components/Swipeable/Swipeable";
 import NavBar from "./components/NavBar/NavBar";
@@ -36,8 +38,6 @@ function App() {
       <div>
         <Swipeable
           onSwipeLeft={() => {
-            console.info("Swiped left!");
-
             if (index >= nbFilmFiltre - 1) {
               setPage(page + 1);
               setIndex(0);
@@ -46,7 +46,6 @@ function App() {
             }
           }}
           onSwipeRight={() => {
-            console.info("Swiped right!");
             const isAlreadyLiked = likedMovie.find(
               (movie) => movie.title === datas.title
             );
@@ -70,17 +69,23 @@ function App() {
           <MovieCard
             activeFiltre={activeFiltre}
             index={index}
-            setIndex={setIndex}
             setPage={setPage}
             page={page}
-            likedMovie={likedMovie}
-            setLikedMovie={setLikedMovie}
             datas={datas}
             setDatas={setDatas}
-            nbFilmFiltre={nbFilmFiltre}
             setNbFilmFiltre={setNbFilmFiltre}
           />
         </Swipeable>
+        <Like
+          setIndex={setIndex}
+          index={index}
+          page={page}
+          setPage={setPage}
+          datas={datas}
+          likedMovie={likedMovie}
+          setLikedMovie={setLikedMovie}
+          nbFilmFiltre={nbFilmFiltre}
+        />
       </div>
       <NavBar
         activeFiltre={activeFiltre}
